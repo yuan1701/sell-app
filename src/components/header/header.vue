ed3da4eea95865bc7295f13cf28cabd30b1d323f
yuanyun <1349281070@qq.com>
2019-02-25 00:10:02 +0800

    商品详情页

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -28,16 +28,29 @@
         <div class="background">
             <img :src="seller.avatar" width="100%" height="100%" alt="">
         </div>
-         <!-- v-show="detailShow" -->
-        <div  class="detail" >
+        <div  class="detail" v-show="detailShow" transition="fade">
             <div class="detail-wrapper clearfix">
                 <div class="detail-main">
                     <h1 class="name">{{seller.name}}</h1>
                     <star class="star-wrapper" :size="48" :score="seller.score"></star>
                     <div class="title">
                         <div class="line"></div>
-                        <div class="tetx">商家优惠</div>
+                        <div class="text">商家优惠</div>
                         <div class="line"></div>
+                    </div>
+                    <ul v-if="seller.supports" class="supports">
+                        <li class="support-item" v-for="item in seller.supports" :key="item.id">
+                            <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+                            <span class="text">{{seller.supports[$index].description}}</span>
+                        </li>
+                    </ul>
+                     <div class="title">
+                        <div class="line"></div>
+                        <div class="text">商家公告</div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="bulletin">
+                        <p class="content">{{seller.bulletin}}</p>
                     </div>
                 </div>
             </div>
@@ -198,7 +211,15 @@ export default {
         z-index 100
         width 100%
         height 100%
-        background rgba(7,17,27,0.8)
+        transition all 0.5s
+        //实现模糊背景
+        backdrop-filter blur(10)
+        &.fade-transition
+            opacity: 1
+            background rgba(7,17,27,0.8)
+        &.fade-enter, &.fade-leave
+            opacity: 0
+            background rgba(7,17,27,0)
         .detail-wrapper
             min-height 100%
             width 100%
@@ -214,6 +235,59 @@ export default {
                     margin-top 18px
                     padding 2px 0
                     text-align center 
+                .title
+                    display flex
+                    width 80%
+                    margin 28px auto 24px auto
+                    .line
+                        flex 1
+                        position relative
+                        top -6px
+                        border-bottom 1px solid rgba(255,255,255,0.2)
+                    .text
+                        padding 0 12px
+                        font-size 14px
+                        font-weight 700
+                .supports
+                    width 80%
+                    margin 0 auto
+                    .support-item
+                        padding 0 12px
+                        margin-bottom 12px
+                        font-size 0
+                        &:last-child
+                            margiin-bottom 0
+                        .icon
+                            display inline-block
+                            width 16px
+                            height 16px
+                            vertical-align top
+                            margin-right 6px
+                            background-size 16px 16px
+                            background-repeat no-repeat
+                            &.decrease
+                                bg-image('decrease_2')
+                            &.discount
+                                bg-image('discount_2')
+                            &.guarantee
+                                bg-image('guarantee_2')
+                            &.invoice
+                                bg-image('invoice_2')
+                            &.special
+                                bg-image('special_2')
+                        .text
+                            font-size 12px
+                            line-height 12px
+                .bulletin
+                    width 80%
+                    margin 0 auto
+                    .content
+                        line-height 24px
+                        padding 0 12px
+                        font-size 12px
+                    
+
+
                   
         .detail-close
             position relative
